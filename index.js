c35f62e9477b50b1e35b8fe3937b36d75acb871c
Angelena Nunez <angelenacorral@gmail.com>
2023-03-02 21:52:50 -0600

    Began creating server routes

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
 import http, { request } from "http"
+import fetch from "node-fetch"
+import { createDeflateRaw } from "zlib"
 
 const server = http.createServer((req, res) =>{
     const url = req.url 
+    let tableData = "<table border="1"><tr><th>ID</th><th>Name</th><th>Username</th><th>Email</th><th>Address</th><th>Phone Number</th></tr>"
     if(url === "/"){
         res.write("Welcome to the Home Page!")
         res.end()
     }
-    if(url === "/message") {
-        res.write("Welcome to the Message Page!")
+    if(url === "/list") {
+        fetch("https://swapi.dev/api/people")
+        .then(res => res.json())
+        .then(data => {
+            createData(data)
+            res.write("Welcome to My List Page")
+            res.end()
+        })
+    }
+    if(url === "/"){
+        res.write("Page Not Found")
         res.end()
     }
+    function createData(data) {
+        data.forEach(element => {
+            tableData+="<tr><td>${element.id}</td><td>${element.name}</td><td>${element.username}</td><td>${element.email}</td><td>${element.address.street}</td><td>${element.phone}</td></tr>"
+        });
+        tableData+="</table>"
+    }
 
-}).listen(8000,console.log("Server is listening on port 8000"))
+}).listen(8090, console.log("Server is listening on port 8090"))
+
+
