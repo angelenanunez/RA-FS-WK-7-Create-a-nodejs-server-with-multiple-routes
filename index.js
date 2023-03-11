2370f7b264dc6cd888cfdb83b411ce9703e9b9b6
Angelena Nunez <angelenacorral@gmail.com>
2023-03-11 17:09:47 -0600

    updated structure

--- index.js
+++ index.js
@@ -3,44 +3,36 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
     const url = req.url
-    console.log(url)   
-    let tableData = "<table><tr><th>ID</th><th>Name</th><th>Username</th><th>Email</th><th>Address</th><th>Phone Number</th></tr>"
+    // console.log(url)   
+    let tableData = "<table border='1'><tr><th>Name</th><th>Username</th><th>Email</th><th>Address</th><th>Phone Number</th></tr>"
     if (url === "/") {
-        res.write("Welcome to the Home Page!")
-         res.end()
+        res.write("<h1>Welcome to the Home Page!</h1> + '<img src=https://i.ibb.co/C85CCV2/pexels-clive-kim-4619986.jpg>'");
+        res.end();
      }
      if (url === "/list") {
          fetch("https://swapi.dev/api/people")
-             .then(res => res.json())
-             .then(data => {
-                 createData(data)
-                 res.write("Welcome to My List Page")
-                 res.end()
-             })
-     }
-     else {
-         res.end("Page not found");
-     }
-    res.end("Hello")
-    function createData(data) {
+             .then((res) => res.json())
+             .then((data) => {
+                console.log(data);
+                console.log(data.results);
+                res.write(tableData)
+                res.end();  
+             });
+    else () {
+        res.end("Page not found");
+    }
+}
+    function createData(data.results) {
         data.forEach(element => {
-            tableData += "<tr><td>${element.id}</td><td>${element.name}</td><td>${element.username}</td><td>${element.email}</td><td>${element.address.street}</td><td>${element.phone}</td></tr>"
+            tableData += `<tr><td>${element.id}</td><td>${element.name}</td><td>${element.username}</td><td>${element.email}</td><td>${element.address.street}</td><td>${element.phone}</td></tr>`
         });
-        tableData += "</table>"
+        tableData += `</table>`
     }
 
-})
+})server.listen(8090, console.log("Server is listening on port " + 8090));
 
-server.listen(8090, console.log("Server is listening on port 8090"))
 
-// const routes = {
-//     home: function (data, res) {
 
-//     },
-//     list: function (data, res) {
 
-//     },
-//     error: function (data, res) {
 
-//     },
-// }
+
