6dd45341fee2c7a80f28a73736a9ee5270959cdd
Angelena Nunez <angelenacorral@gmail.com>
2023-03-11 18:15:10 -0600

    fixed spacing

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const server = http.createServer((req, res) => {
     const url = req.url
     // console.log(url)   
     let tableData = "<table border='1'><tr><th>Name</th><th>Username</th><th>Email</th><th>Address</th><th>Phone Number</th></tr>"
+    
     if (url === "/") {
         res.write("<h1>Welcome to the Home Page!</h1> + '<img src=https://i.ibb.co/C85CCV2/pexels-clive-kim-4619986.jpg>'");
         res.end();
      }
+     
      if (url === "/list") {
          fetch("https://swapi.dev/api/people")
              .then((res) => res.json())
@@ -18,16 +20,19 @@ const server = http.createServer((req, res) => {
                 res.write(tableData)
                 res.end();  
      })};
-    else () => {
+    
+     else () => {
     res.write("Page not found");
     res.end();
     }
+    
     function createData(data) {
         data.forEach(element => {
             tableData += `<tr><td>${element.id}</td><td>${element.name}</td><td>${element.username}</td><td>${element.email}</td><td>${element.address.street}</td><td>${element.phone}</td></tr>`
         });
         tableData += `</table>`
     }
+    
 })server.listen(8090, console.log("Server is listening on port " + 8090));
 
 
